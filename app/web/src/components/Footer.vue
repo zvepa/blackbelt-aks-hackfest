fcb78aab628d414a6997085469bed2c3bac10f3d
zvepa <zvepa@hotmail.com>
2019-01-16 16:40:02 +1100

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Github Rocks!!!
+        Github Rocks?!?!
       </div>
       <div class="col-lg-6">
       </div>
